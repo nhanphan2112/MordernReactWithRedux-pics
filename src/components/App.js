84bdbb5bf58f10467c73e6e18baff003fb4f465a
Nhan Phan <nphanacc@gmail.com>
2021-06-07 13:45:06 -0700

    95-Install Axios to access back end API

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,16 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term){
-    console.log(term);
-  }
+	onSearchSubmit(term) {
+		console.log(term);
+	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: "10px" }}>
-				<SearchBar onSubmit={this.onSearchSubmit}/>
+				<SearchBar onSubmit={this.onSearchSubmit} />
 			</div>
 		);
 	}
